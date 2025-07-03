e1382c4ffbfd274a50a9beacc6947883b56e3381
burak-ezer <burakezerrr@gmail.com>
2025-07-04 00:35:47 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buton = document.querySelector(".change-color");
+
+buton.addEventListener("click", function() {
+  let color = getRandomHexColor();
+  document.querySelector("body").setAttribute("style", `background-color: ${color};`)
+  document.querySelector(".color").innerHTML = color;
+});
