667d788615fd82c8866a2539ba001ed24b0a1a0a
burak-ezer <burakezerrr@gmail.com>
2025-07-04 01:22:27 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (
+    event.target.elements.email.value.length == 0 ||
+    event.target.elements.password.value.length == 0
+  ) {
+    alert("All form fields must be filled in");
+  }
+
+  event.target.reset();
+});
